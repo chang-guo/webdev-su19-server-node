120092e888dfd0fb5615bb7c63b8dd18924d4745
chang-guo <guo.chan@husky.neu.edu>
2019-08-03 11:27:18 -0400

    add mongodb dependency

--- data/db.js
+++ data/db.js
@@ -31,4 +31,8 @@ module.exports = function () {
     };
 
     mongoose.connect(connectionString,options);
+
+    let db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'connection error:'));
 };
