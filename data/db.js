057facb8ec6d3d6788e722298f440512f73a8650
chang-guo <guo.chan@husky.neu.edu>
2019-08-03 11:45:42 -0400

    add uri

--- data/db.js
+++ data/db.js
@@ -6,33 +6,11 @@ module.exports = function () {
     connectionString += databaseName;
 
     if(process.env.MLAB_USERNAME_WEBDEV) {
-        var username = process.env.MLAB_USERNAME_WEBDEV;
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        let username = process.env.MLAB_USERNAME_WEBDEV;
+        let password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
         connectionString += '@ds113122.mlab.com:13122/heroku_lfscl362';
     }
 
-    let options = {
-        server : {
-            socketOptions : {
-                keepAlive : 300000,
-                connectTimeoutMS : 30000
-            }
-        },
-        replset : {
-            socketOptions : {
-                keepAlive : 300000,
-                connectTimeoutMS : 30000
-            }
-        },
-        db: {
-            useNewUrlParser: true
-        }
-    };
-
-    mongoose.connect(connectionString,options);
-
-    let db = mongoose.connection;
-
-    db.on('error', console.error.bind(console, 'connection error:'));
+    mongoose.connect(connectionString,{useNewUrlParser: true});
 };
